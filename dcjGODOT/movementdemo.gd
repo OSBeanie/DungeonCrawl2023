4299c8c00167503747f9978074e2b60757bcc2e9
plexsoup <plexsoup@gmail.com>
2023-04-04 20:07:13 -0700

    movement tweens

--- dcjGODOT/movementdemo.gd
+++ dcjGODOT/movementdemo.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 var direction = 0
+var step_distance = 1.0
+
 var moving = false
 func _process(delta):
 	if Input.is_action_just_pressed("turn_left"):
@@ -20,10 +22,12 @@ func _process(delta):
 		$AudioStreamPlayer.play()
 		#separate method: get_parent().get_node("movetimer").start()
 		#original movement: 
-		self.position += (Vector3.FORWARD * 1).rotated(Vector3.UP, self.rotation.y)
-		#var tween = self.create_tween()
-		#tween.tween_property(self, "position", (Vector3.FORWARD * 1).rotated(Vector3.UP, self.rotation.y), .1)
-		#tween.tween_interval(.1)
-		#tween.tween_property(self, "position", (Vector3.FORWARD * 1).rotated(Vector3.UP, self.rotation.y), .1)
+		#self.position += (Vector3.FORWARD * 1).rotated(Vector3.UP, self.rotation.y)
+		var tween = self.create_tween()
+		var dirVector = -self.get_camera_transform().basis.z * step_distance
+		
+		tween.tween_property(self, "position", position + dirVector, .2)
+		tween.tween_interval(.1)
+		tween.tween_property(self, "position", position + (2.0*dirVector), .2)
 	else:
 		!moving

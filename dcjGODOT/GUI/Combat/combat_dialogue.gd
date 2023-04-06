3cc139ab7c6278b2af8b6959ccba18ffc4bc14b8
plexsoup <plexsoup@gmail.com>
2023-04-06 01:50:32 -0700

    added more words

--- dcjGODOT/GUI/Combat/combat_dialogue.gd
+++ dcjGODOT/GUI/Combat/combat_dialogue.gd
@@ -14,6 +14,9 @@ and the player takes damage after each exchange.
 
 extends Control
 
+# might need to refine this list to make it more transparent and fair.
+# antonym, synonym, tangentially related concept
+
 # for each word, 3 possible responses are listed.
 # response 0 = neutral, 1 = seraph, 2 = siann
 enum response_types { NEUTRAL, SERAPH, SIANN }
@@ -52,6 +55,70 @@ var dialogue_options = {
 		"solve":["create","understand","optimize"],
 		"relax":["stress","rest","enjoy"],
 		"laugh":["cry","smile","empathize"],
+		"clean":["dirty","tidy","sanitize"],
+		"fast":["slow","quick","efficient"],
+		"happy":["sad","joyful","content"],
+		"hot":["cold","warm","spicy"],
+		"big":["small","large","important"],
+		"red":["green","crimson","blood"],
+		"fly":["fall","soar","travel"],
+		"light":["dark","bright","energy"],
+		"hard":["easy","solid","difficult"],
+		"soft":["hard","smooth","gentle"],
+		"sharp":["dull","pointed","smart"],
+		"round":["square","circular","whole"],
+		"high":["low","tall","elevated"],
+		"deep":["shallow","profound","submerged"],
+		"long":["short","lengthy","enduring"],
+		"loud":["quiet","noisy","intense"],
+		"sweet":["sour","sugary","pleasant"],
+		"young":["old","youthful","new"],
+		"rich":["poor","wealthy","abundant"],
+		"simple":["complex","easy","clear"],
+		"open":["closed","wide","honest"],
+		"strong":["weak","powerful","resilient"],
+		"black":["white","dark","mystery"],
+		"smooth":["rough","sleek","calm"],
+		"clear":["unclear","transparent","obvious"],
+		"thick":["thin","dense","viscous"],
+		"wet":["dry","moist","drenched"],
+		"near":["far","close","adjacent"],
+		"begin":["end","start","commence"],
+		"old":["new","aged","vintage"],
+		"poor":["rich","destitute","scarce"],
+		"fasten":["unfasten","secure","tie"],
+		"cheap":["expensive","inexpensive","low-priced"],
+		"expand":["contract","extend","widen"],
+		"dark":["light","dim","gloomy"],
+		"quiet":["loud","silent","serene"],
+		"calm":["agitated","relaxed","composed"],
+		"rough":["smooth","coarse","unpolished"],
+		"arrive":["depart","reach","show up"],
+		"accept":["reject","approve","agree"],
+		"attack":["defend","assault","strike"],
+		"beautiful":["ugly","attractive","pretty"],
+		"beginning":["ending","start","commencement"],
+		"bold":["timid","courageous","brave"],
+		"careful":["careless","cautious","prudent"],
+		"dangerous":["safe","hazardous","risky"],
+		"day":["night","dawn","morning"],
+		"empty":["full","vacant","unfilled"],
+		"failure":["success","defeat","loss"],
+		"give":["take","offer","provide"],
+		"ignorant":["knowledgeable","unaware","uninformed"],
+		"inside":["outside","interior","indoors"],
+		"junior":["senior","younger","subordinate"],
+		"knowledge":["ignorance","wisdom","information"],
+		"maximum":["minimum","greatest","highest"],
+		"nearby":["distant","close","adjacent"],
+		"present":["absent","current","existent"],
+		"question":["answer","inquiry","interrogation"],
+		"single":["married","unmarried","sole"],
+		"top":["bottom","peak","summit"],
+		"up":["down","above","ascend"],
+		"victory":["defeat","success","triumph"],
+		"weak":["strong","feeble","frail"],
+		
 }
 
 var current_question : String
@@ -98,7 +165,7 @@ func _on_response_chosen(responseString):
 		hide()
 		combat_resolved.emit()
 		$HappyNoise.start()
-		return
+		
 
 	generate_dialog()
 
